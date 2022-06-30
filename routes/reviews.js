3db1bc706aa7efbda4cdaccbf1ec38b0b52d2934
psison <sisonpatrick29@yahoo.com>
2022-06-30 16:18:54 -0500

    refactor code

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,12 +4,6 @@ import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-// GET -- localhost:3000/reviews
-router.get('/', isLoggedIn, reviewsCtrl.index)
-
-// GET -- localhost:3000/reviews/new
-router.get('/new', isLoggedIn, reviewsCtrl.new)
-
 // POST -- localhost:3000/reviews/:id
 router.post('/:id', isLoggedIn, reviewsCtrl.create)
 
